ca233ee620d783e8f109f1669e8273a035c52d0f
KarelKrnlius <karelkarelkornelius@gmail.com>
2026-05-06 09:29:05 +0700

    Memperbaiki line yang tidak terpakai

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UsersSeeder extends Seeder
 {
@@ -13,22 +14,28 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('users')->delete(); // Clear existing data
+        
         // Admin account
         User::create([
+            'id' => 1,
             'name' => 'Admin User',
             'email' => 'admin@simas.com',
             'password' => Hash::make('admin123'),
             'email_verified_at' => now(),
-            'role_id' => 1, 
+            'role_id' => 1,
+            'is_active' => true,
         ]);
             
         // Staff account
         User::create([
+            'id' => 2,
             'name' => 'Staff User',
             'email' => 'staff@simas.com',
             'password' => Hash::make('staff123'),
             'email_verified_at' => now(),
-            'role_id' => 2, 
+            'role_id' => 2,
+            'is_active' => true,
         ]);
     }
 }
